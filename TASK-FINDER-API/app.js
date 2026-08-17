466354003f628cc121d09b0b701059c8559ee41c
giftdivine <giftdivine787@gmail.com>
2026-08-17 19:37:36 +0100

    Task-finder-Api

---
+++ TASK-FINDER-API/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+const tasks = [
+    {
+        id: 1,
+        name: "Learn Monodb"
+    },
+    {
+        id: 2,
+        name: "Learn Express"
+    },
+]
+
+
+app.get("/tasks/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const result = tasks.find((task) => {
+        return task.id === id;
+    });
+
+    res.json(result);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
